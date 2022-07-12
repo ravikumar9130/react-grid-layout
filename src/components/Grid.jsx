b23eb6e5c84f140ac3111a4e8228b94e3974aa23
ravikumar <ravikumarwolfd9130@gmail.com>
2022-07-12 14:17:05 +0530

    styled-component removed

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,5 +1,4 @@
 import { Responsive, WidthProvider } from "react-grid-layout";
-import styled from "styled-components";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
@@ -10,18 +9,7 @@ const layout = [
   { i: "c4", x: 3, y: 0, w: 1, h: 1 ,static:false},
   { i: "c5", x: 4, y: 0, w: 1, h: 1 ,static:false}
 ];
-const GridItemWrapper = styled.div`
-   background: #cfcaca;
-`;
 
-const GridItemContent = styled.div`
-   padding: 8px;
-   color:black;
-`;
-
-const Root = styled.div`
- padding: 16px;
-`;
 
 const getLayouts = () => {
   const savedLayouts = localStorage.getItem("grid-layout");
@@ -35,7 +23,7 @@ export const Grid = () => {
 };
 
 return (
-  <Root>
+  <div className="root">
     <ResponsiveGridLayout
     
       layouts={getLayouts()}
@@ -48,22 +36,22 @@ return (
 
       onLayoutChange={handleLayoutChange}
     >
-      <GridItemWrapper key="c1">
-        <GridItemContent>content 1</GridItemContent>
-      </GridItemWrapper>
-      <GridItemWrapper key="c2">
-        <GridItemContent>content 2</GridItemContent>
-      </GridItemWrapper>
-      <GridItemWrapper key="c3">
-        <GridItemContent>content 3</GridItemContent>
-      </GridItemWrapper>
-      <GridItemWrapper key="c4">
-        <GridItemContent>content 4</GridItemContent>
-      </GridItemWrapper>
-      <GridItemWrapper key="c5">
-        <GridItemContent>content 5</GridItemContent>
-      </GridItemWrapper>
+      <div className="grid-wrapper" key="c1">
+        <div className="grid-item-content">content 1</div>
+      </div>
+      <div className="grid-wrapper" key="c2">
+        <div>content 2</div>
+      </div>
+      <div className="grid-wrapper" key="c3">
+        <div>content 3</div>
+      </div>
+      <div className="grid-wrapper" key="c4">
+        <div>content 4</div>
+      </div>
+      <div className="grid-wrapper" key="c5">
+        <div>content 5</div>
+      </div>
     </ResponsiveGridLayout>
-  </Root>
+  </div>
 );
 };
